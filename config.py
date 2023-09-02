71fa6a3f2ef9be5ac085f1f73bb8d8c70d9ad809
cxe-123 <87938416+cxe-123@users.noreply.github.com>
2023-09-03 00:26:49 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-1.crehh31zya2u.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "chanxiner-employee"
+customregion = "us-east-1"
+
